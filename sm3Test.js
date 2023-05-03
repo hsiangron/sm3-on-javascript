36fd5b81f4974af33c592720fb6cd0fad40e412d
codesplace <2793173855@qq.com>
2023-05-03 16:43:25 +0800

    测试了一个JS样例

---
+++ sm3Test.js
@@ -0,0 +1,9 @@
+import { Person } from './core/sm3.js';
+/*
+一个测试样例
+输入：这是一段需要加密的原文
+输出：7503FE4B33EF72F4775A743E1B8D7B94527E10E3F55B204958C2637AF55F7261
+*/
+
+const john = new Person('John', 30);
+john.sayHello(); // 输出：Hello, my name is John and I am 30 years old.
