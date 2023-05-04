603ea1135779593916ca394a786691945c3693b7
codesplace <2793173855@qq.com>
2023-05-04 10:19:00 +0000

    添加了一些函数，仍然有一些bug需要修改

--- core/sm3.js
+++ core/sm3.js
@@ -1,5 +1,6 @@
 export class SM3 {
     constructor() {
+    
     }
 
     //将二进制转换为十六进制
@@ -106,6 +107,166 @@ export class SM3 {
         return result;
     }
 
+    //实现异或
+    XOR(str1, str2) {
+        let result = "";
+        str1 = this.Hex2Bin(str1);
+        str2 = this.Hex2Bin(str2);
+        for (let i = 0; i < str1.length; i++) {
+            result += str1[i] ^ str2[i];
+        }
+        return result;
+    }
+
+    //实现与
+    AND(str1, str2) {
+        let result = "";
+        str1 = this.Hex2Bin(str1);
+        str2 = this.Hex2Bin(str2);
+        for (let i = 0; i < str1.length; i++) {
+            result += str1[i] & str2[i];
+        }
+        return result;
+    }
+
+    //实现或
+    OR(str1, str2) {
+        let result = "";
+        str1 = this.Hex2Bin(str1);
+        str2 = this.Hex2Bin(str2);
+        for (let i = 0; i < str1.length; i++) {
+            result += str1[i] | str2[i];
+        }
+        return result;
+    }
+
+    
+    //实现非
+    NOR(str1, str2) {
+        let result = "";
+        str1 = this.Hex2Bin(str1);
+        str2 = this.Hex2Bin(str2);
+        for (let i = 0; i < str1.length; i++) {
+            result += ~(str1[i] | str2[i]);
+        }
+        return result;
+    }
+
+    //实现对2^32次方取模
+    modAdd(str1, str2) {
+        let res1 = HexToBin(str1);
+        let res2 = HexToBin(str2);
+        let temp = '0';
+        let res = "";
+        for (let i = res1.length - 1; i >= 0; i--) {
+            res = (res1[i] ^ res2[i]) ^ temp + res;
+            if (res1[i] & res2[i] == '1') 
+                temp = '1';
+            else if (res1[i] ^ res2[i] == '1') 
+                    temp = '1' & temp;
+                else 
+                temp = '0';
+        }
+        return BinToHex(res);
+    }
+
+    //实现置换功能P0（X）   
+    P0(str) {
+        return XOR(XOR(str, LeftShift(str, 9)), LeftShift(str, 17));
+    }   
+
+    //实现置换功能P1（X）
+    P1(str) {
+        return XOR(XOR(str, LeftShift(str, 15)), LeftShift(str, 23));
+    }
+
+    //返回Tj常量值的函数实现
+    Tj(i) {
+        if (i >= 0 && i <= 15) {
+            return "79cc4519";
+        } else if (i >= 16 && i <= 63) {
+            return "7a879d8a";
+        }
+    }
+
+    //实现布尔函数FF功能
+    FF(X, Y, Z, i) {
+        if (i >= 0 && i <= 15) {
+            return XOR(XOR(X, Y), Z);
+        } else if (i >= 16 && i <= 63) {
+            return OR(OR(AND(X, Y), AND(X, Z)), AND(Y, Z));
+        }
+    }
+
+    //实现布尔函数GG功能
+    GG(X, Y, Z, i) {
+        if(i >= 0 && i <= 15) {
+            return XOR(XOR(X, Y), Z);
+        }
+        else if(i >= 16 && i <= 63) {
+            return OR(OR(AND(X, Y), AND(~X, Z)), AND(~X, Y));
+        }
+    }
+    
+    //实现消息拓展函数功能
+    extension(str) {
+        let res = str;
+        for (let i = 16; i < 68; i++) {
+            res += this.XOR(this.XOR(this.P1(this.XOR(this.XOR(res.substr((i-16)*8,8), res.substr((i - 9) * 8, 8)), this.LeftShift(res.substr((i - 3) * 8, 8), 15))), this.LeftShift(res.substr((i - 13) * 8, 8), 7)), res.substr((i - 6) * 8, 8));
+        }
+
+        for (let i = 0; i < 64; i++) {
+            res += this.XOR(res.substr(i * 8, 8), res.substr((i + 4) * 8, 8));
+        }
+
+        return res;
+    }
+
+    //消息压缩函数
+    compress(str1, str2) {
+        let A1 = str1.substr(0, 8);
+        let B1 = str1.substr(8, 8);
+        let C1 = str1.substr(16, 8);
+        let D1 = str1.substr(24, 8);
+        let E1 = str1.substr(32, 8);
+        let F1 = str1.substr(40, 8);
+        let G1 = str1.substr(48, 8);
+        let H1 = str1.substr(56, 8);
+        SS1 = "", SS2 = "", TT1 = "", TT2 = "";
+        for(let i=0; i<64; i++) {
+            SS1= LeftShift(modAdd(modAdd(LeftShift(A1, 12), E1), LeftShift(Tj(i), i)), 7);
+            SS2= XOR(SS1, LeftShift(A1, 12));
+            TT1= modAdd(modAdd(modAdd(modAdd(FF(A1, B1, C1, i), D1), SS2), this.extension(str2).substr(8*i, 8)), this.Tj(i));
+            TT2= modAdd(modAdd(modAdd(modAdd(GG(E1, F1, G1, i), H1), SS1), this.extension(str2).substr(8*i, 8)), this.Tj(i+4));
+            D1=C1;
+            C1= LeftShift(B1, 9);
+            B1=A1;
+            A1=TT1;
+            H1=G1;
+    		G1 = LeftShift(F1, 19);
+		    F1 = E1;        
+            E1 = this.P0(TT2);
+        }
+        return this.modAdd(str1, this.Bin2Hex(A1+B1+C1+D1+E1+F1+G1+H1));
+    }
+
+    //迭代压缩函数实现
+    iteration(str){
+        let num = str.length / 128;
+        let V = "7380166F4914B2B9172442D7DA8A0600A96F30BC163138AAE38DEE4DB0FB0E4E";
+        for (let i = 0; i < num; i++) {
+            let  B = str.substr(i * 128, 128);
+            let extensionB = this.extension(B);
+            let compressB = this.compress(extensionB, V);
+            V = XOR(V, compressB);
+        }
+        return V;
+    }
 
 }
+
+
+
+
+    
 
--- sm3Test.js
+++ sm3Test.js
@@ -1,14 +1,18 @@
-import {SM3} from './core/sm3.js';
+import { SM3 } from './core/sm3.js';
 
 /*
 一个测试样例
 输入：helloworld
 输出：c70c5f73da4e8b8b73478af54241469566f6497e16c053a03a0170fa00078283
 */
-
 let sm3 = new SM3();
-let msg = "helloworld";
-let hash = sm3.sm3(msg);
-console.log(hash); // "66C7F0F4B9E9755B9F2D6AEFFB8BAA0B8D1F6AB8"
-
+let str = "helloworld";
+for (let num = 0; num < 2; num++) {
+		let paddingValue = sm3.padding(str[num]);
+		let result = sm3.iteration(paddingValue);
+		cout << "hash:" << endl;
+		for (let i = 0; i < 8; i++) {
+			console.log(result.substr(i * 8, 8));
+		}
+}
 
